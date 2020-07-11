0a8a81d78c49779c025bae1579bd3b2405ca01a7
Mick Zeller <mickzeller@gmail.com>
2020-07-11 13:39:17 -0600

    Finishing Accordion.js component

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -2,6 +2,13 @@ import React, { useState } from 'react';
 
 const Accordion = ({items}) => {
 
+  //                    CLASS_COMPONENT             VS            FUNCTION_COMPONENT
+  // Initialization: state = {activeIndex: 0}                       useState(0);
+  // Reference:      this.state.activeIndex                         activeIndex;
+  // Updates:        this.setState({activeIndex: 10})             setActiveIndex(10);
+
+  // const [ PIECE_OF_STATE, F(X)_TO_CHANGE_STATE ] = useState( INIT_VALUE_FOR_THIS_PIECE_OF_STATE )
+  // hook to allow use of state - this syntax utilizes 'array destructuring'
   const [activeIndex, setActiveIndex] = useState(null)
 
   // helper function
@@ -10,15 +17,18 @@ const Accordion = ({items}) => {
   }
 
   const renderedItems = items.map((item, index) => {
+    // variable to control which dropdown is active
+    const active = index === activeIndex ? 'active' : ''
+
     return <React.Fragment key={item.title}>
       <div
-        className="title active"
+        className={`title ${active}`}
         onClick={() => onTitleClick(index)}
       >
         <i className="dropdown icon"/>
         {item.title}
       </div>
-      <div className="content active">
+      <div className={`content ${active}`}>
         <p>{item.content}</p>
       </div>
     </React.Fragment>
@@ -26,7 +36,6 @@ const Accordion = ({items}) => {
   return (
     <div className="ui styled accordion">
       {renderedItems}
-      <h1>{activeIndex}</h1>
     </div>
   )
 }
